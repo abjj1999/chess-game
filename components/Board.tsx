8e09dfb93e724e83a4dfda985d2f97664a1a3c30
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-12-06 13:55:52 -0600

    pieces are moving and caputring other pieces

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,40 +1,67 @@
-import { Chess } from "chess.js"
-import { useEffect, useState } from "react"
-import styles from "./board.module.scss"
-import { p0, pw, pb } from "utils/chess-utils"
+import { useEffect, useState } from "react";
+import styles from "./board.module.scss";
+import { p0, pw, pb, chess, getBoard } from "utils/chess-utils";
 export default function Board() {
-    const [pieces, setPieces] = useState(new Array(8).fill(0).map(() => new Array(8).fill("")))
+    const [pieces, setPieces] = useState(
+        new Array(8).fill(0).map(() => new Array(8).fill(""))
+    );
+    const [highlighted, setHighlighted] = useState<string[]>([]);
     useEffect(() => {
-        const chess = new Chess()
-        const b = chess.ascii().split('\n').slice(1, 9).map(rank => rank.slice(5, 27).split("  "));
-        setPieces(b)
-
-    }, [])
+        setPieces(getBoard());
+    }, []);
     return (
         <div className={styles.board}>
-            {
-                new Array(8).fill(0).map((_, i) => 
-                (<div className={styles.row} key={i}>
+            {new Array(8).fill(0).map((_, i) => (
+                <div className={styles.row} key={i}>
                     {new Array(8).fill(0).map((_, j) => {
                         let p = pieces[i][j];
+                        let c = "";
                         if (p === ".") {
                             p = "";
-                        }else if(p.match(/[A-Z]/)){
-                            p = pw[p0.indexOf(p.toLowerCase())]
-                        }else {
-                            p = pb[p0.indexOf(p)]
+                        } else if (p.match(/[A-Z]/)) {
+                            p = pw[p0.indexOf(p.toLowerCase())];
+                            c = "w";
+                        } else {
+                            p = pb[p0.indexOf(p)];
+                            c = "b";
                         }
+                        const square = `${"abcdefgh".charAt(j)}${8 - i}`;
                         return (
-                        <div className={[styles.col, (i+j)%2 === 0? styles.w : styles.b].join(" ")} key={`${i}, ${j}`}>
-                            {p}
-                        </div>
+                            <div
+                                className={[
+                                    styles.col,
+                                    (i + j) % 2 === 0 ? styles.w : styles.b,
+                                    p && chess.turn() == c && styles.pointer,
+                                    highlighted.slice(1).includes(square) && styles.highlighted,
+                                ].join(" ")}
+                                key={`${i}, ${j}`}
+                                onClick={() => {
+                                    if (highlighted.slice(1).includes(square)) {
+                                        chess.move({to: square, from: highlighted[0]});
+                                        setPieces(getBoard());
+                                        setHighlighted([]);
+                                    }else if(p && chess.turn() == c) {
 
-                        )
-                    }
-                    )}
-                    
-                </div>))
-            }
+                                        const mvs = chess.moves({
+                                            //@ts-ignore
+                                            square: square,
+                                            verbose: true,
+                                            //@ts-ignore
+                                        }) as Move[];
+                                        // console.log(mvs);
+                                        setHighlighted([square, ...mvs.map(({ to }) => to)]);
+                                    } 
+                                    else {
+                                        setHighlighted([]);
+                                    }
+                                }}
+                            >
+                                {p}
+                            </div>
+                        );
+                    })}
+                </div>
+            ))}
         </div>
-    )
+    );
 }
